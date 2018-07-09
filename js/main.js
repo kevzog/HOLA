474c78c7a5913273498f4f6ea536a0c7452aa1aa
kevzog <kevh.bis@gmail.com>
2018-07-09 18:35:59 -0300

    css en el chat y arreglo de creacion de elementos dinamicos en el html

--- js/main.js
+++ js/main.js
@@ -18,13 +18,22 @@ frasesPorHora[m][s]*/
 setInterval(function(){
 
   d = new Date();
+  h = d.getHours();
   m = d.getMinutes();
   s = d.getSeconds();
   console.log(m,s);
 
   if (messageInTime[m][s]) {
     var msg = document.createElement("div");
-    msg.innerHTML = messageInTime[m][s];
+    msg.classList.add("msg");
+    var chat = document.createElement("p");
+    chat.classList.add("chat");
+    var time = document.createElement("p");
+    time.classList.add("time");
+    time.innerHTML = h + ":" + m;
+    chat.innerHTML = messageInTime[m][s];
+    msg.appendChild(chat);
+    msg.appendChild(time);
     document.body.appendChild(msg);
   }
 
